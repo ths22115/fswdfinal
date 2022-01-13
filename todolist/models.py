6ea1a97a5e4c80284b5be27f3c3594cb9f3cbee2
milesthomas <ths22115@towerhill.org>
2022-01-13 10:54:52 -0500

    Initial commit

---
+++ todolist/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.db import models
+
+# Create your models here
+class User(AbstractUser):
+    pass
+
+class Task(models.Model):
+    CATAGORIES = (
+        ('A', 'Assignment'),
+        ('C', 'Chore'),
+        ('G', 'Goal'),
+        ('C', 'Self Care'),
+        ('W', 'Work'),
+        ('P', 'People')
+    )
+    REPEATS = (
+        ('N', 'Never'),
+        ('H', 'Hourly'),
+        ('D', 'Daily'),
+        ('W', 'Weekly'),
+        ('M', 'Monthly'),
+        ('Y', 'Yearly')
+    )
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    job = models.TextField()
+    category = models.CharField(max_length=1, choices=CATAGORIES)
+    # startdate = models.DateTimeField(default=timezone.now)
+    # enddate = models.DateTimeField(blank=True)
+    # remainingtime = models.DurationField(blank=True)
+    # due = models.BooleanField(default=False)
+    done = models.BooleanField(default=False)
+    repeat = models.CharField(max_length=1, choices=REPEATS)
+
+    def __str__(self):
+        if (self.repeated != 'N'):
+            return f"{self.title} ({self.get_catagory_display()}, Repeats {self.get_repeated_display()})"
+        return f"{self.title} ({self.get_catagory_display()})"
---
+++ todolist/views.py
@@ -0,0 +1,99 @@
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from .models import User, Task
+
+# Create your views here.
+# task.objects.filter(done=false)
+# task.objects.all()
+#
+# tasks = task.objects.all()
+# done_tasks = tasks.filter(done=True)
+#
+# model mail?
+
+def index(request): #done
+
+    # Authenticated users view their inbox
+    if request.user.is_authenticated:
+        return render(request, "todolist/inbox.html")
+
+    # Everyone else is prompted to sign in
+    else:
+        return HttpResponseRedirect(reverse("login"))
+
+
+@login_required
+def page(request, page): #done
+    # Filter tasks returned based on page
+    if page == "list":
+        tasks = Task.objects.filter(
+            user=request.user, done=False
+        )
+    elif page == "finished":
+        tasks = Task.objects.filter(
+            user=request.user, done=True
+        )
+    else:
+        return JsonResponse({"error": "Invalid mailbox."}, status=400)
+
+    # Return emails in reverse chronologial order
+    tasks = tasks.order_by("timestamp").all()
+    return JsonResponse([task.serialize() for task in tasks], safe=False)
+
+def login_view(request): #done
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "todolist/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "todolist/login.html")
+
+
+def logout_view(request): #done
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request): #done
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "todolist/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(name, email, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "todolist/register.html", {
+                "message": "Email address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "todolist/register.html")
